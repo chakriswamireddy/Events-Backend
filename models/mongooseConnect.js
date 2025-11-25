dd7edafc99538ad9a75d322bd7a89ab698ceb743
chakriswamireddy <iamthechakri@gmail.com>
2025-11-25 22:01:20 +0530

    ref- mongo url changed

--- models/mongooseConnect.js
+++ models/mongooseConnect.js
@@ -3,8 +3,6 @@ const mongoose = require('mongoose');
 const mongooseConnect = () => {
     
     mongoose.connect(process.env.MONGO_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
       }).then(() => {
         console.log('Connected to MongoDB');
       }).catch(err => console.error('MongoDB connection error:', err));
